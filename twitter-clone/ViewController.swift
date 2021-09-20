08874141ca1af69e0235b2ebb3af2228cfc1e460
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-20 14:29:42 +0000

    Initial Commit

---
+++ twitter-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-clone
+//
+//  Created by Kwame Agyenim - Boateng on 20/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
